ad0fdec0920c6ad377e58aea555249a29bf4413f
mistermurph <jacobanthonymurphy@gmail.com>
2020-12-20 14:55:02 -0500

    Added FB Pixel. Added GA and Pixel to blank layout. (oops)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     @if(App::environment('production') || App::environment('staging'))
+    {{-- Google Analytics --}}
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-173526157-1"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
@@ -11,23 +12,34 @@
         gtag('config', 'UA-173526157-1');
     </script>
     @endif
+    @if(App::environment('production'))
+    {{-- Facebook Pixel --}}
+    <script>
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '854950435339757');
+    fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+    src="https://www.facebook.com/tr?id=854950435339757&ev=PageView&noscript=1"
+    /></noscript>
+    @endif
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" type="image/png" href="{{asset('/image/icon.png')}}"/>
-    <!-- CSRF Token -->
+    {{-- CSRF Token --}}
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>@yield('title')</title> 
-
-    <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
-
-    <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <style>
     :root {
